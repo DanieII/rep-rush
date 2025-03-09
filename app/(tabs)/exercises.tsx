5fafc7247d7880f8b8cb016974b58190134ba938
DanieII <danielbogdanov1748@gmail.com>
2025-03-09 12:02:29 +0200

    Use FlatList instead of ScrollView for exercises list

--- app/(tabs)/exercises.tsx
+++ app/(tabs)/exercises.tsx
@@ -2,8 +2,7 @@ import AddExercise from "@/components/AddExercise";
 import Exercise from "@/components/Exercise";
 import { useExercises } from "@/hooks/useExercises";
 import { useState } from "react";
-import { View, Button, StyleSheet } from "react-native";
-import { ScrollView } from "react-native";
+import { View, Button, StyleSheet, FlatList } from "react-native";
 
 export default function Exercises() {
   const { exercises, addExercise, removeExercise } = useExercises();
@@ -11,17 +10,14 @@ export default function Exercises() {
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <View style={styles.exercises}>
-          {exercises.map((exercise) => (
-            <Exercise
-              key={exercise.id}
-              exercise={exercise}
-              removeExercise={removeExercise}
-            />
-          ))}
-        </View>
-      </ScrollView>
+      <FlatList
+        contentContainerStyle={styles.exercises}
+        data={exercises}
+        renderItem={({ item }) => (
+          <Exercise exercise={item} removeExercise={removeExercise} />
+        )}
+        keyExtractor={(item) => item.id}
+      ></FlatList>
       <AddExercise
         modalVisible={modalVisible}
         setModalVisible={setModalVisible}
@@ -39,7 +35,6 @@ const styles = StyleSheet.create({
     padding: 30,
   },
   exercises: {
-    flex: 1,
     gap: 20,
   },
 });
