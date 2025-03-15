13d102a30a397613c76e795c5632d16537450e09
DanieII <danielbogdanov1748@gmail.com>
2025-03-15 11:04:02 +0200

    Add main logic for the workout page

--- app/workouts/[id].tsx
+++ app/workouts/[id].tsx
@@ -1,12 +1,72 @@
-import { useLocalSearchParams } from "expo-router";
+import CustomButton from "@/components/CustomButton";
+import { getWorkout } from "@/lib/workouts";
+import { router, useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
-export default function WorkoutScreen() {
+export default function Workout() {
   const { id } = useLocalSearchParams();
+  const workout = getWorkout(id as string);
+  const [isStarted, setIsStarted] = useState(false);
+  const [phase, setPhase] = useState<"exercise" | "rest">("exercise");
+  const [timer, setTimer] = useState(workout?.exerciseDuration || 0);
+  const [exerciseIndex, setExerciseIndex] = useState(0);
+
+  if (!workout) {
+    return <Text>Workout not found</Text>;
+  }
+
+  const toggleStart = () => {
+    setIsStarted((prev) => !prev);
+  };
+
+  const startNextPhase = () => {
+    if (phase === "exercise") {
+      setTimer(workout.restDuration);
+      setPhase("rest");
+      setExerciseIndex((prev) => prev + 1);
+    } else if (phase === "rest") {
+      setTimer(workout.exerciseDuration);
+      setPhase("exercise");
+    }
+  };
+
+  useEffect(() => {
+    if (!isStarted) return;
+
+    if (exerciseIndex === workout.exercises.length) {
+      setIsStarted(false);
+      router.back();
+
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setTimer((prev) => {
+        if (prev === 0) {
+          startNextPhase();
+        }
+
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [isStarted, phase, exerciseIndex]);
 
   return (
     <View>
-      <Text>Workout: {id}</Text>
+      <Text>Workout: {workout.title}</Text>
+      <Text>{timer}</Text>
+      <Text>
+        Exercise:
+        {workout.exercises[exerciseIndex]?.title ||
+          workout.exercises[exerciseIndex - 1].title}
+      </Text>
+      <CustomButton
+        title={isStarted ? "Pause" : "Start"}
+        onPress={toggleStart}
+      />
     </View>
   );
 }
--- lib/workouts.ts
+++ lib/workouts.ts
@@ -8,6 +8,12 @@ export const getWorkouts = (): TWorkout[] => {
   return workouts;
 };
 
+export const getWorkout = (id: string): TWorkout | undefined => {
+  const workouts = getWorkouts();
+
+  return workouts.find((workout) => workout.id === id);
+};
+
 export const saveWorkouts = (workouts: TWorkout[]) => {
   storage.set("workouts", JSON.stringify(workouts));
 };
